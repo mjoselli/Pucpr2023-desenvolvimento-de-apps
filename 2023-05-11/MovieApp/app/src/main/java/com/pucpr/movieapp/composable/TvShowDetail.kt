ca1eeb914ce1e07321e333082ac2ab0677931bf8
mjoselli <mejoselli@gmail.com>
2023-05-23 20:21:15 -0300

    Aula 23/05/2023

--- 2023-05-11/MovieApp/app/src/main/java/com/pucpr/movieapp/composable/TvShowDetail.kt
+++ 2023-05-11/MovieApp/app/src/main/java/com/pucpr/movieapp/composable/TvShowDetail.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.unit.dp
 import com.pucpr.movieapp.model.TvShow
 
